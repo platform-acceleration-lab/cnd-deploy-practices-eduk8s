54ed34008f54aae7ae8e6a451eafdc5f03825099
Ryan Schneider <ryan@heptio.com>
2021-03-18 14:11:43 -0400

    add workaround for kubect delete of educates resources and add image side load

--- deploy/kind.sh
+++ deploy/kind.sh
@@ -14,8 +14,6 @@ kind delete cluster --name "${NAME}" > /dev/null 2>&1 || true
 echo "===== Creating cluster"
 kind create cluster --name "${NAME}" --config $DIR/kind-config.yaml
 
-echo "===== Loading image into cluster"
-kind load docker-image --name "${NAME}" ${NAME}:latest
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 sleep 10
 kubectl wait --namespace ingress-nginx \
